c938d1357643b2b4809b54f6e970602443a0d5c3
mizuki20020105 <66400695+mizuki20020105@users.noreply.github.com>
2020-06-11 18:16:42 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module ChatSpace
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    confing.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
   end
 end
