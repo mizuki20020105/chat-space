5ac96ab480abaa4851338089c520f9ee192bfa1a
mizuki20020105 <66400695+mizuki20020105@users.noreply.github.com>
2020-06-21 15:36:31 +0900

    incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ? ',"%#{params[:keyword]}%"]).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -7,14 +17,11 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :edit  
+    end
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   end
 end
-
-  
-
